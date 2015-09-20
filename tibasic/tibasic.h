cada13c8c47de72def5ef9d1d7d3d33465b41454
Michael Welch <michaelgwelch@gmail.com>
2015-09-20 16:41:52 -0500

    Initial Commit

---
+++ tibasic/tibasic.h
@@ -0,0 +1,19 @@
+//
+//  tibasic.h
+//  tibasic
+//
+//  Created by Michael Welch on 9/20/15.
+//  Copyright © 2015 Michael Welch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for tibasic.
+FOUNDATION_EXPORT double tibasicVersionNumber;
+
+//! Project version string for tibasic.
+FOUNDATION_EXPORT const unsigned char tibasicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tibasic/PublicHeader.h>
+
+
